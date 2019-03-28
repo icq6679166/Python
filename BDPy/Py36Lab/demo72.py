a49df2d4605d7e6d930d515968a89a8daec49df3
icq6679166 <icq6679166@gmail.com>
2019-03-28 17:03:47 +0800

    BDPy201903271703Day4

---
+++ BDPy/Py36Lab/demo72.py
@@ -0,0 +1,23 @@
+class Group(object):
+    def __init__(self):
+        print("initial function")
+        self.counter = 0
+
+    def __iter__(self):
+        print("start to iterate")
+        return self
+
+    def __next__(self):
+        if self.counter < 10:
+            self.counter += 1
+            return self.counter
+        else:
+            raise StopIteration()
+
+    pass
+
+
+g1 = Group()
+for i in g1:
+    print("***", i)
+print(type(g1))
